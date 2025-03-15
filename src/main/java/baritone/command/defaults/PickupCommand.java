cabaletta/baritone

✨ Command to pickup items

src/main/java/baritone/command/defaults/PickupCommand.java
@@ -1,0 +1,82 @@
+/*
+ * This file is part of Baritone.
+ *
+ * Baritone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Baritone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package baritone.command.defaults;
+
+import baritone.api.IBaritone;
+import baritone.api.command.Command;
+import baritone.api.command.argument.IArgConsumer;
+import baritone.api.command.datatypes.ItemById;
+import baritone.api.command.exception.CommandException;
+import baritone.api.command.helpers.TabCompleteHelper;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class PickupCommand extends Command {
+
+    public PickupCommand(IBaritone baritone) {
+        super(baritone, "pickup");
+    }
+
+    @Override
+    public void execute(String label, IArgConsumer args) throws CommandException {
+        Set<Item> collecting = new HashSet<>();
+        while (args.hasAny()) {
+            Item item = args.getDatatypeFor(ItemById.INSTANCE);
+            collecting.add(item);
+        }
+        if (collecting.isEmpty()) {
+            baritone.getFollowProcess().pickup(stack -> true);
+            logDirect("Picking up all items");
+        } else {
+            baritone.getFollowProcess().pickup(stack -> collecting.contains(stack.getItem()));
+            logDirect("Picking up these items:");
+            collecting.stream().map(Item.REGISTRY::getNameForObject).map(ResourceLocation::toString).forEach(this::logDirect);
+        }
+    }
+
+    @Override
+    public Stream<String> tabComplete(String label, IArgConsumer args) throws CommandException {
+        while (args.has(2)) {
+            if (args.peekDatatypeOrNull(ItemById.INSTANCE) == null) {
+                return Stream.empty();
+            }
+            args.get();
+        }
+        return args.tabCompleteDatatype(ItemById.INSTANCE);
+    }
+
+    @Override
+    public String getShortDesc() {
+        return "Pickup items";
+    }
+
+    @Override
+    public List<String> getLongDesc() {
+        return Arrays.asList(
+                "Usage:",
+                "> pickup - Pickup anything",
+                "> pickup <item1> <item2> <...> - Pickup certain items"
+        );
+    }
+}

src/main/java/baritone/process/FollowProcess.java
@@ -19,6 +19,7 @@ package baritone.process;
 
 import baritone.Baritone;
 import baritone.api.pathing.goals.Goal;
+import baritone.api.pathing.goals.GoalBlock;
 import baritone.api.pathing.goals.GoalComposite;
 import baritone.api.pathing.goals.GoalNear;
 import baritone.api.pathing.goals.GoalXZ;
@@ -27,6 +28,8 @@ import baritone.api.process.PathingCommand;
 import baritone.api.process.PathingCommandType;
 import baritone.utils.BaritoneProcessHelper;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.List;
@@ -43,6 +46,7 @@ public final class FollowProcess extends BaritoneProcessHelper implements IFollo
 
     private Predicate<Entity> filter;
     private List<Entity> cache;
+    private boolean into;
 
     public FollowProcess(Baritone baritone) {
         super(baritone);
@@ -57,11 +61,14 @@ public final class FollowProcess extends BaritoneProcessHelper implements IFollo
 
     private Goal towards(Entity following) {
         BlockPos pos;
-        if (Baritone.settings().followOffsetDistance.value == 0) {
+        if (Baritone.settings().followOffsetDistance.value == 0 || into) {
             pos = new BlockPos(following);
         } else {
             GoalXZ g = GoalXZ.fromDirection(following.getPositionVector(), Baritone.settings().followOffsetDirection.value, Baritone.settings().followOffsetDistance.value);
             pos = new BlockPos(g.getX(), following.posY, g.getZ());
+        }
+        if (into) {
+            return new GoalBlock(pos);
         }
         return new GoalNear(pos, Baritone.settings().followRadius.value);
     }
@@ -112,6 +119,13 @@ public final class FollowProcess extends BaritoneProcessHelper implements IFollo
     @Override
     public void follow(Predicate<Entity> filter) {
         this.filter = filter;
+        this.into = false;
+    }
+
+    @Override
+    public void pickup(Predicate<ItemStack> filter) {
+        this.filter = e -> e instanceof EntityItem && filter.test(((EntityItem) e).getItem());
+        this.into = true;
     }
 
     @Override

wujinjun-MC/baritone_1.21.5

src/main/java/baritone/command/defaults/PickupCommand.java
@@ -1,0 +1,83 @@
+/*
+ * This file is part of Baritone.
+ *
+ * Baritone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Baritone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package baritone.command.defaults;
+
+import baritone.api.IBaritone;
+import baritone.api.command.Command;
+import baritone.api.command.argument.IArgConsumer;
+import baritone.api.command.datatypes.ItemById;
+import baritone.api.command.exception.CommandException;
+import baritone.api.command.helpers.TabCompleteHelper;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class PickupCommand extends Command {
+
+    public PickupCommand(IBaritone baritone) {
+        super(baritone, "pickup");
+    }
+
+    @Override
+    public void execute(String label, IArgConsumer args) throws CommandException {
+        Set<Item> collecting = new HashSet<>();
+        while (args.hasAny()) {
+            Item item = args.getDatatypeFor(ItemById.INSTANCE);
+            collecting.add(item);
+        }
+        if (collecting.isEmpty()) {
+            baritone.getFollowProcess().pickup(stack -> true);
+            logDirect("Picking up all items");
+        } else {
+            baritone.getFollowProcess().pickup(stack -> collecting.contains(stack.getItem()));
+            logDirect("Picking up these items:");
+            collecting.stream().map(BuiltInRegistries.ITEM::getKey).map(ResourceLocation::toString).forEach(this::logDirect);
+        }
+    }
+
+    @Override
+    public Stream<String> tabComplete(String label, IArgConsumer args) throws CommandException {
+        while (args.has(2)) {
+            if (args.peekDatatypeOrNull(ItemById.INSTANCE) == null) {
+                return Stream.empty();
+            }
+            args.get();
+        }
+        return args.tabCompleteDatatype(ItemById.INSTANCE);
+    }
+
+    @Override
+    public String getShortDesc() {
+        return "Pickup items";
+    }
+
+    @Override
+    public List<String> getLongDesc() {
+        return Arrays.asList(
+                "Usage:",
+                "> pickup - Pickup anything",
+                "> pickup <item1> <item2> <...> - Pickup certain items"
+        );
+    }
+}

src/main/java/baritone/process/FollowProcess.java
@@ -19,6 +19,7 @@ package baritone.process;
 
 import baritone.Baritone;
 import baritone.api.pathing.goals.Goal;
+import baritone.api.pathing.goals.GoalBlock;
 import baritone.api.pathing.goals.GoalComposite;
 import baritone.api.pathing.goals.GoalNear;
 import baritone.api.pathing.goals.GoalXZ;
@@ -27,11 +28,14 @@ import baritone.api.process.PathingCommand;
 import baritone.api.process.PathingCommandType;
 import baritone.api.utils.BetterBlockPos;
 import baritone.utils.BaritoneProcessHelper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
 
 /**
  * Follow an entity
@@ -42,6 +46,7 @@ public final class FollowProcess extends BaritoneProcessHelper implements IFollo
 
     private Predicate<Entity> filter;
     private List<Entity> cache;
+    private boolean into; // walk straight into the target, regardless of settings
 
     public FollowProcess(Baritone baritone) {
         super(baritone);
@@ -56,11 +61,14 @@ public final class FollowProcess extends BaritoneProcessHelper implements IFollo
 
     private Goal towards(Entity following) {
         BlockPos pos;
-        if (Baritone.settings().followOffsetDistance.value == 0) {
+        if (Baritone.settings().followOffsetDistance.value == 0 || into) {
             pos = following.blockPosition();
         } else {
             GoalXZ g = GoalXZ.fromDirection(following.position(), Baritone.settings().followOffsetDirection.value, Baritone.settings().followOffsetDistance.value);
             pos = new BetterBlockPos(g.getX(), following.position().y, g.getZ());
+        }
+        if (into) {
+            return new GoalBlock(pos);
         }
         return new GoalNear(pos, Baritone.settings().followRadius.value);
     }
@@ -114,6 +122,13 @@ public final class FollowProcess extends BaritoneProcessHelper implements IFollo
     @Override
     public void follow(Predicate<Entity> filter) {
         this.filter = filter;
+        this.into = false;
+    }
+
+    @Override
+    public void pickup(Predicate<ItemStack> filter) {
+        this.filter = e -> e instanceof ItemEntity && filter.test(((ItemEntity) e).getItem());
+        this.into = true;
     }
 
     @Override